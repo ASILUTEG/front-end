c73a33dbabbf3bb5fb04fce8c3fe448fae6f1122
ali hassen <asiliteg@gmail.com>
2020-12-26 14:36:55 +0200

    js task 2

---
+++ java script sec2/js/main.js
@@ -0,0 +1,11 @@
+// q1 i=4
+// q2 j error un defind
+// q3 key already declared will not contenue
+var num=Number(prompt("inter the number ?"))
+var reslut=1;
+for (var i = num; i >=1 ; i--) {
+    reslut*=i
+    console.log(`the result is :${reslut}`)
+
+}
+console.log(`the result is :${reslut}`)
